a53fe5d0e6517dcf49ccb7d162f900d9105937dd
HartmanAndrew <51465030+HartmanAndrew@users.noreply.github.com>
2021-03-03 18:32:08 -0800

    Created new code for PID control
    Made a new folder called HypoxiaChamber that holds the new code and renamed the files within.

---
+++ Arduino Code/HypoxiaChamber/ControlFunctions.ino
@@ -0,0 +1,179 @@
+
+
+void CommandParse(String input){
+  int selection = 0;
+  String CO2Reading;
+  if(input.substring(0,4) == "open"){
+      selection = input.substring(5).toInt();
+      switch(selection){
+        case 0:
+          digitalWrite(SOL_O2, 1);
+          digitalWrite(SOL_CO2, 1);
+          digitalWrite(SOL_Ex, 1);
+          break;
+        case 1:
+          digitalWrite(SOL_O2, 1);
+          break;
+        case 2:
+          digitalWrite(SOL_CO2,1);
+          break;
+        case 3:
+          digitalWrite(SOL_Ex, 1);
+          break;
+        default:
+          Serial.print(selection);
+          Serial.println("Error, invalid solenoid to open");
+      }
+      Serial.println(input);
+    } 
+    else if(input.substring(0,5) == "close") {
+      selection = input.substring(6).toInt();
+      switch(selection){
+        case 0:
+          digitalWrite(SOL_O2, 0);
+          digitalWrite(SOL_CO2, 0);
+          digitalWrite(SOL_Ex, 0);
+          break;
+        case 1:
+          digitalWrite(SOL_O2, 0);
+          break;
+        case 2:
+          digitalWrite(SOL_CO2,0);
+          break;
+        case 3:
+          digitalWrite(SOL_Ex, 0);
+          break;
+        default:
+          Serial.println("Error, invalid solenoid to Close");
+      }
+       Serial.println(input);
+    } 
+    else if(input.substring(0,2) == "O2") {
+      O2Setpoint = input.substring(3).toFloat();
+    }
+    else if(input.substring(0,3) == "CO2") {
+      CO2Serial->print(input.substring(4));
+      if (CO2Serial->available()) {
+        CO2Reading = CO2Serial->readStringUntil('\n');
+        Serial.println("CO2 Command Return" + CO2Reading);
+      }
+    }
+    else if(input.substring(0,5) == "Debug"){
+      Debug = (Debug+1)%2;
+    }
+    else {
+       Serial.println(input);
+       Serial.println("Invalid Command");
+    }
+}
+
+double ControlO2(double O2Percent, double O2Set, double Kp, double Ki, double Kd){
+  static double Output;
+  static PID myPID(&O2Percent, &Output, &O2Set, Kp, Ki, Kd, DIRECT);
+  static int Setup = 0;
+  if(!Setup) {
+    myPID.SetMode(AUTOMATIC);
+    myPID.SetOutputLimits(0, UpperO2Time);
+    Setup = 1;
+  }
+  myPID.Compute();
+  digitalWrite(LED_BUILTIN, 1);
+  digitalWrite(SOL_Ex, 1);
+  digitalWrite(SOL_O2, 1);
+  delay(Output);
+  digitalWrite(LED_BUILTIN, 0);
+  digitalWrite(SOL_O2, 0);
+  digitalWrite(SOL_Ex, 0);
+  return Output;
+}
+
+double ControlCO2(double CO2Percent, double CO2Set, double Kp, double Ki, double Kd){
+  static double Output;
+  static PID myPID(&CO2Percent, &Output, &CO2Set, Kp, Ki, Kd, DIRECT);
+  static int Setup = 0;
+  if(!Setup) {
+    myPID.SetMode(AUTOMATIC);
+    myPID.SetOutputLimits(0, UpperCO2Time);
+    Setup = 1;
+  }
+  myPID.Compute();
+  digitalWrite(LED_BUILTIN, 1);
+  digitalWrite(SOL_Ex, 1);
+  digitalWrite(SOL_CO2, 1);
+  delay(Output);
+  digitalWrite(LED_BUILTIN, 0);
+  digitalWrite(SOL_CO2, 0);
+  digitalWrite(SOL_Ex, 0);
+  return Output;
+}
+
+int readings( double *O2Percent, double *CO2Percent, float *Temp, float *Humidity, float *Pressure) {
+  String CO2Reading, O2Reading, HReading, PReading;
+  CO2Serial->println("Z");
+  int retval = 1;
+  if (O2Serial->available()) {    
+    O2Reading = O2Serial->readStringUntil('\n');
+    if(O2Reading.length() < 42 && O2Reading.length() > 35){
+      *O2Percent = O2Reading.substring(26,32).toDouble();
+      *Temp = O2Reading.substring(12,16).toFloat();
+      if(Debug) Serial.println("O2:"+O2Reading);
+      if(Debug) Serial.println("O2%:"+ (String)*O2Percent);
+      if(Debug) Serial.println("O2 T:"+ (String)*Temp);
+    } else {
+      if(Debug) Serial.println("Bad O2:" + O2Reading);
+      *O2Percent = 100;
+      retval = 0;
+    }
+  }
+  delay(50);
+  if (CO2Serial->available()) {
+    CO2Reading = CO2Serial->readStringUntil('\n');
+    if((CO2Reading.length() < 9) && (CO2Reading.substring(0,1) != "E")) {
+      *CO2Percent = CO2Reading.substring(2,7).toFloat()/1000;
+      if(Debug) Serial.println("CO2:"+CO2Reading);
+      if(Debug) Serial.println("CO2 %:" + (String)*CO2Percent);
+    } else {
+      if(Debug) Serial.println("CO2 Bad="+CO2Reading);
+      *CO2Percent = 100;
+    }
+  }
+
+  delay(50);
+  CO2Serial->println("H");
+  delay(50);
+  if (CO2Serial->available()) {
+    HReading = CO2Serial->readStringUntil('\n');
+    if((HReading.length() < 9) && (HReading.substring(0,1) != "E")) {
+      *Humidity = HReading.substring(2,7).toFloat()/10;
+      if(Debug) Serial.println("H:" + HReading);
+      if(Debug) Serial.println("H %:" + (String)*Humidity);
+    } else {
+      if(Debug) Serial.println("H Bad:" +HReading);
+    }
+  }
+  delay(50);
+  CO2Serial->println("B");
+  delay(50);
+  if (CO2Serial->available()) {
+    PReading = CO2Serial->readStringUntil('\n');
+    if((PReading.length() < 9) && (PReading.substring(0,1) != "E")) {
+      *Pressure = PReading.substring(2,7).toFloat()/10;
+      if(Debug) Serial.println("P: " + PReading);
+      if(Debug) Serial.println("P #: " + (String)*Pressure);
+    } else {
+      if(Debug) Serial.println("P Bad: " +PReading);
+    }
+  }
+  return retval;
+
+}
+
+int CheckTime(unsigned long *previous, unsigned long delay){
+  unsigned long currentTime = millis();
+  int retval = 0;
+  if(currentTime - *previous >= delay){
+    retval = 1;
+    *previous = currentTime;
+  }
+  return retval;
+}
---
+++ Arduino Code/HypoxiaChamber/HypoxiaChamber.ino
@@ -0,0 +1,129 @@
+/*
+  *
+  * The First test software for parsing O2 and CO2 Readings
+  * 
+  * Setup:
+  * 
+  *   O2 Rx - pin 19 - RX1
+  *   O2 Tx - pin 18 - TX1
+  *   
+  *   CO2 RX - pin 17 - RX2
+  *   CO2 Tx - Pin 16 - TX2
+  *   
+  * Open up the serial monitor with a baud rate of 9600 and type commands to control the system
+  * 
+  * Commands:
+  *   "open x" - opens solenoids 1-3 anything other than 1-3 will open all of them
+  *   "close x" - closes solenoid x where x is 1-3 anything other than 1-3 closes all of them
+  */
+#include <stdio.h>
+#include <string.h>
+#include <PID_v1.h>
+
+//#define Debug 1
+
+#define SOL_O2 22
+#define SOL_CO2 23
+#define SOL_Ex 24
+
+#define UpperO2Time 1200   // Max time the solenoid can open for at once
+#define UpperCO2Time 1200
+
+#define readTime 7 // Time between sensor readings and solenoids, in seconds
+
+double CO2Setpoint = 5.0;
+double O2Setpoint = 5.0;
+
+double O2Kp=2, O2Ki=5, O2Kd=1;
+
+double CO2Kp=2, CO2Ki=5, CO2Kd=1;
+
+int Debug = 1;
+String inputString;         // a String to hold incoming data
+float Temperature;
+double O2Percent;
+double CO2Percent;
+double O2Solenoid;
+double CO2Solenoid;
+float Temp;
+float Humidity;
+float Pressure;
+int CO2PPM;
+
+//bool ReadSerial = false; // Outdated, used with the timer
+
+HardwareSerial *O2Serial = &Serial1;
+HardwareSerial *CO2Serial = &Serial2;
+
+void setup()
+{
+   pinMode(SOL_O2, OUTPUT);
+   pinMode(SOL_CO2, OUTPUT);
+   pinMode(SOL_Ex, OUTPUT);
+   pinMode(LED_BUILTIN, OUTPUT);
+   
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Native USB only
+  }
+  Serial.println("Serial Connected");
+  Serial.println("O2, CO2, Temp, Humid, Pressure, O2Solenoid, CO2Solenoid");
+
+  // set the data rate for the two Serial ports: Serial1=O2, Serial2=CO2
+  O2Serial->begin(9600);
+  CO2Serial->begin(9600);
+}
+
+void loop() // run over and over
+{  
+  static unsigned long previous = millis();
+  // Used to track if when readings() is called it receives a good O2 reading.
+  // 1: Good last reading
+  // 0: Bad last reading
+  //The goal is to always have read a poor reading in the off cycle and not calculate based on it so that the good reading comes through at the Delay time
+  int goodReading = 0; 
+  
+  if(CheckTime(&previous, readTime*1000)){ //readTime is a #define above that is multiplied by 1000 to get the millisecond equivalent
+      goodReading = readings(&O2Percent, &CO2Percent, &Temp, &Humidity, &Pressure);
+      O2Solenoid = ControlO2(O2Percent, O2Setpoint, O2Kp, O2Ki, O2Kd);
+      delay(50);
+      CO2Solenoid = ControlCO2(CO2Percent, CO2Setpoint, CO2Kp, CO2Ki, CO2Kd);
+      Serial.print(O2Percent);
+      Serial.print(",");
+      Serial.print(CO2Percent);
+      Serial.print(",");
+      Serial.print(Temp);
+      Serial.print(",");
+      Serial.print(Humidity);
+      Serial.print(",");
+      Serial.print(Pressure);
+      Serial.print(",");
+      Serial.print(O2Solenoid);
+      Serial.print(",");
+      Serial.print(CO2Solenoid);
+      Serial.print("\n");
+  } 
+  if(goodReading == 1){ // implemented to avoid the issue of every other O2 reading being extra long and bad. This only happened when increasing the delay time over 1s for some reason
+      //Serial.println("Offset:");
+      goodReading = readings(&O2Percent, &CO2Percent, &Temp, &Humidity, &Pressure);
+  }
+}
+
+
+//Used to capture the serial command inputs sent by the user over the serial 1 line.
+void serialEvent() {
+  
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      CommandParse(inputString);
+      inputString = "";
+    }
+  }
+}
